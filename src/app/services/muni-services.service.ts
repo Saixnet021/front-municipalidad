bb8723dc208a9f284b163a0fbdabb13099669dfe
Ing. Anderson F. <fernandezanderson562@gmail.com>
2025-12-03 10:22:31 -0500

    Fix hardcoded localhost URL in MuniServicesService

--- src/app/services/muni-services.service.ts
+++ src/app/services/muni-services.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MuniServicesService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/tramites';
+  private apiUrl = `${environment.apiUrl}/tramites`;
 
   constructor(private http: HttpClient) { }
 
